e747ee2cfa94b5f3caebd6acff3a0b3fcd68137d
Mikkel Birch <mib@kvalitetsit.dk>
2023-05-12 14:28:47 +0200

    kittyact chart

--- build/push-repo.sh
+++ build/push-repo.sh
@@ -8,8 +8,8 @@ git config --global user.name "Github Actions"
 git config --global user.email "development@kvalitetsit.dk"
 git clone https://github.com/KvalitetsIT/helm-repo
 cd helm-repo
-cp ../../service-*.tgz service
+cp ../../kittyact-*.tgz kittyact
 helm repo index . --url https://raw.githubusercontent.com/KvalitetsIT/helm-repo/master/
 git add -A
-git commit -m "Adding new service chart"
+git commit -m "Adding new kittyact chart"
 git push https://$1@github.com/KvalitetsIT/helm-repo
