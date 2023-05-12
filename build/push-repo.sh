73b68aba29e393ebcb65ce6c360cb93b6e17b4f0
Mikkel Birch <mib@kvalitetsit.dk>
2023-05-12 14:37:16 +0200

    folder mayne now?

--- build/push-repo.sh
+++ build/push-repo.sh
@@ -8,7 +8,7 @@ git config --global user.name "Github Actions"
 git config --global user.email "development@kvalitetsit.dk"
 git clone https://github.com/KvalitetsIT/helm-repo
 cd helm-repo
-cp ../../kittyact-*.tgz kittyact
+cp ../../kittyact-*.tgz kittyact/
 helm repo index . --url https://raw.githubusercontent.com/KvalitetsIT/helm-repo/master/
 git add -A
 git commit -m "Adding new kittyact chart"
